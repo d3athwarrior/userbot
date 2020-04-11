85255bc63a36b049cd4334571ca3deb1fe18070b
Anish Kumar <anishsk@users.noreply.github.com>
2020-04-11 19:50:34 +0530

    Fix a missing comment

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -35,7 +35,7 @@ LOGS = getLogger(__name__)
 
 #if version_info[0] < 3 or version_info[1] < 8:
 #    LOGS.info("You MUST have a python version of at least 3.8."
-              "Multiple features depend on this. Bot quitting.")
+#              "Multiple features depend on this. Bot quitting.")
 #    quit(1)
 
 # Check if the config was edited by using the already used variable.
