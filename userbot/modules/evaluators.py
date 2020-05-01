3f6bcb5aba1f1637186ccd7da426b96edb3eba43
anishsk <anish114@gmail.com>
2020-05-01 23:29:20 +0530

    Re-enable linux shell command handling
    Signed-off-by: anishsk <anish114@gmail.com>

--- userbot/modules/evaluators.py
+++ userbot/modules/evaluators.py
@@ -132,65 +132,65 @@ from userbot.events import register
 #             "Exec query " + codepre + " was executed successfully")
 
 
-# @register(outgoing=True, pattern="^.term(?: |$)(.*)")
-# async def terminal_runner(term):
-#     """ For .term command, runs bash commands and scripts on your server. """
-#     curruser = getuser()
-#     command = term.pattern_match.group(1)
-#     try:
-#         from os import geteuid
-#         uid = geteuid()
-#     except ImportError:
-#         uid = "This ain't it chief!"
+@register(outgoing=True, pattern="^.term(?: |$)(.*)")
+async def terminal_runner(term):
+    """ For .term command, runs bash commands and scripts on your server. """
+    curruser = getuser()
+    command = term.pattern_match.group(1)
+    try:
+        from os import geteuid
+        uid = geteuid()
+    except ImportError:
+        uid = "This ain't it chief!"
 
-#     if term.is_channel and not term.is_group:
-#         await term.edit("`Term commands aren't permitted on channels!`")
-#         return
+    if term.is_channel and not term.is_group:
+        await term.edit("`Term commands aren't permitted on channels!`")
+        return
 
-#     if not command:
-#         await term.edit("``` Give a command or use .help term for \
-#             an example.```")
-#         return
+    if not command:
+        await term.edit("``` Give a command or use .help term for \
+            an example.```")
+        return
 
-#     if command in ("userbot.session", "config.env"):
-#         await term.edit("`That's a dangerous operation! Not Permitted!`")
-#         return
+    if command in ("userbot.session", "config.env"):
+        await term.edit("`That's a dangerous operation! Not Permitted!`")
+        return
 
-#     process = await asyncio.create_subprocess_shell(
-#         command,
-#         stdout=asyncio.subprocess.PIPE,
-#         stderr=asyncio.subprocess.PIPE)
-#     stdout, stderr = await process.communicate()
-#     result = str(stdout.decode().strip()) \
-#         + str(stderr.decode().strip())
+    process = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) \
+        + str(stderr.decode().strip())
 
-#     if len(result) > 4096:
-#         output = open("output.txt", "w+")
-#         output.write(result)
-#         output.close()
-#         await term.client.send_file(
-#             term.chat_id,
-#             "output.txt",
-#             reply_to=term.id,
-#             caption="`Output too large, sending as file`",
-#         )
-#         remove("output.txt")
-#         return
+    if len(result) > 4096:
+        output = open("output.txt", "w+")
+        output.write(result)
+        output.close()
+        await term.client.send_file(
+            term.chat_id,
+            "output.txt",
+            reply_to=term.id,
+            caption="`Output too large, sending as file`",
+        )
+        remove("output.txt")
+        return
 
-#     if uid is 0:
-#         await term.edit("`" f"{curruser}:~# {command}" f"\n{result}" "`")
-#     else:
-#         await term.edit("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
+    if uid is 0:
+        await term.edit("`" f"{curruser}:~# {command}" f"\n{result}" "`")
+    else:
+        await term.edit("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
 
-#     if BOTLOG:
-#         await term.client.send_message(
-#             BOTLOG_CHATID,
-#             "Terminal Command " + command + " was executed sucessfully",
-#         )
+    if BOTLOG:
+        await term.client.send_message(
+            BOTLOG_CHATID,
+            "Terminal Command " + command + " was executed sucessfully",
+        )
 
 
 # CMD_HELP.update({"eval": ".eval 2 + 3\nUsage: Evalute mini-expressions."})
 # CMD_HELP.update(
 #    {"exec": ".exec print('hello')\nUsage: Execute small python scripts."})
-# CMD_HELP.update(
-#     {"term": ".term ls\nUsage: Run bash commands and scripts on your server."})
+CMD_HELP.update(
+    {"term": ".term ls\nUsage: Run bash commands and scripts on your server."})
