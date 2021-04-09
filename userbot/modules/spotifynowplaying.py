ad597397d3d3d915cab03d950c9a80abd219b47a
d3athwarrior <anish114@gmail.com>
2021-04-09 23:49:14 +0530

    Log error messages to the botlog chat and console

--- userbot/modules/spotifynowplaying.py
+++ userbot/modules/spotifynowplaying.py
@@ -1,11 +1,18 @@
 import os
-import requests
-from spotipy.exceptions import SpotifyException
-from userbot.modules.sql_helper.spotifypublished import SpotifyPlayed, is_song_published, publish_song
-from spotipy import Spotify, util
-from userbot import CMD_HELP, LOGS, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_LIST_CHAT_ID, SPOTIFY_PLAY_TIME_BEFORE_PUBLISH, SPOTIFY_QUERY_DELAY, SPOTIFY_USERNAME, bot
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
+
+import requests
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from spotipy import Spotify, util
+from spotipy.exceptions import SpotifyException
+from telethon.errors.rpcbaseerrors import BadRequestError
+from userbot import (BOTLOG, BOTLOG_CHATID, CMD_HELP, LOGS, LOGSPAMMER,
+                     SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET,
+                     SPOTIFY_LIST_CHAT_ID, SPOTIFY_PLAY_TIME_BEFORE_PUBLISH,
+                     SPOTIFY_QUERY_DELAY, SPOTIFY_USERNAME, bot)
+from userbot.modules.sql_helper.spotifypublished import (SpotifyPlayed,
+                                                         is_song_published,
+                                                         publish_song)
 
 spotify_client = None
 if (SPOTIFY_USERNAME not in (None, "") and SPOTIFY_CLIENT_ID not in (None, "")
@@ -73,9 +80,15 @@ if (SPOTIFY_USERNAME not in (None, "") and SPOTIFY_CLIENT_ID not in (None, "")
                 else:
                     publish_song(song_detail=song_detail)
                 open('image.png', 'wb').write(requests.get(url=album_art_URL).content)
-                await bot.send_file(SPOTIFY_LIST_CHAT_ID, file = 'image.png', caption=message_body +
+                try:
+                    await bot.send_file(SPOTIFY_LIST_CHAT_ID, file = 'image.png', caption=message_body +
                                             '\n\n<a href="' + browser_link + '">Open Browser Link</a>' +
                                             '\n<a href="' + mobile_link + '">Open Mobile Link</a>', parse_mode='html')
+                except BadRequestError as ex:
+                    if BOTLOG and BOTLOG_CHATID:
+                        LOGS.error("Invalid username specified in config" + ex.message)
+                        await bot.send_message("Error in publishing songs to the specified channel " + SPOTIFY_LIST_CHAT_ID +  " . Error is: " + ex.message)
+
                 os.remove('image.png')
     scheduler = AsyncIOScheduler()
     scheduler.add_job(queryNowPlaying, 'interval', seconds=SPOTIFY_QUERY_DELAY, misfire_grace_time=4, coalesce=True)
